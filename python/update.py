03563763fe709ecd9954a008487fb63cf6e1e31b
Kaoru Chisen <cordx56@cordx.net>
2021-04-19 23:44:33 +0900

    update for Red screen

--- python/update.py
+++ python/update.py
@@ -4,7 +4,7 @@ import base64
 import traceback
 from PIL import Image, ImageDraw, ImageFont
 from flask import request, jsonify
-from epdpil import EPD
+from epd5in83bc import EPD
 
 epd = EPD()
 epddata = { "image": "init.png", "texts": [] }
@@ -20,6 +20,20 @@ def update():
     except Exception as e:
         return jsonify({ "status": False, "message": "Draw error!\n" + str(e) + "\n" + traceback.format_exc() }), 500
     return jsonify({ "status": True, "image": img_base64 })
+
+def split_red_black(img):
+    size = img.size
+    b = Image.new("RGB", size)
+    r = Image.new("RGB", size)
+    for x in range(size[0]):
+        for y in range(size[1]):
+            r, g, b = img.getpixel((x, y))
+            if g == 0 and b == 0:
+                if r == 0:
+                    b.putpixel((x, y), (0, 0, 0))
+                elif r == 255:
+                    r.putpixel((x, y), (0, 0, 0))
+    return (b, r)
 
 def draw():
     global epddata
@@ -39,8 +53,9 @@ def draw():
                 text["text"] if "text" in text else "",
                 color
             )
+        b, r = split_red_black(img)
         epd.init()
-        epd.display_frame(epd.get_frame_buffer(img.transpose(Image.ROTATE_180)))
+        epd.display(epd.getbuffer(b), epd.getbuffer(r))
         epd.sleep()
         img.save(buffer, format="png")
     return base64.b64encode(buffer.getvalue()).decode("utf-8")
