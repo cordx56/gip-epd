03adf05ef1357b01ac7f65c5b6c036dc3df85b9b
Kaoru Chisen <cordx56@cordx.net>
2021-04-20 11:06:24 +0900

    save epddate to file

--- python/update.py
+++ python/update.py
@@ -1,42 +1,52 @@
 import os
 import io
 import base64
+import json
 import traceback
 from PIL import Image, ImageDraw, ImageFont
 from flask import request, jsonify
-from epd5in83bc import EPD
+#from epd5in83bc import EPD
 
-epd = EPD()
-epddata = { "image": "", "texts": [] }
+#epd = EPD()
+def read_epddata():
+    if not os.path.isfile("epddata.json"):
+        return { "image": "", "texts": [] }
+    with open("epddata.json") as f:
+        epddata = json.load(f)
+    return epddata
+def write_epddata(epddata):
+    with open("epddata.json", "w") as f:
+        json.dump(epddata, f)
 
 def update():
-    global epddata
-    if "image" in request.json:
-        epddata["image"] = request.json["image"]
-    if "texts" in request.json and isinstance(request.json["texts"], list):
-        epddata["texts"] = request.json["texts"]
     try:
-        img_base64 = draw()
+        epddata = read_epddata()
+        if "image" in request.json:
+            epddata["image"] = request.json["image"]
+        if "texts" in request.json and isinstance(request.json["texts"], list):
+            epddata["texts"] = request.json["texts"]
+        write_epddata(epddata)
+        img_base64 = draw(epddata)
     except Exception as e:
         return jsonify({ "status": False, "message": "Draw error!\n" + str(e) + "\n" + traceback.format_exc() }), 500
     return jsonify({ "status": True, "image": img_base64 })
 
 def split_red_black(img):
-    size = img.size
-    b = Image.new("RGB", size)
-    r = Image.new("RGB", size)
+    img_rgb = img.convert('RGB')
+    size = img_rgb.size
+    imgb = Image.new("RGB", size)
+    imgr = Image.new("RGB", size)
     for x in range(size[0]):
         for y in range(size[1]):
-            r, g, b = img.getpixel((x, y))
+            r, g, b = img_rgb.getpixel((x, y))
             if g == 0 and b == 0:
                 if r == 0:
-                    b.putpixel((x, y), (0, 0, 0))
+                    imgb.putpixel((x, y), (0, 0, 0))
                 elif r == 255:
-                    r.putpixel((x, y), (0, 0, 0))
-    return (b, r)
+                    imgr.putpixel((x, y), (0, 0, 0))
+    return (imgb, imgr)
 
-def draw():
-    global epddata
+def draw(epddata):
     buffer = io.BytesIO()
     with Image.open(os.path.join("./image", epddata["image"])) as img:
         drw = ImageDraw.Draw(img)
@@ -54,12 +64,12 @@ def draw():
                 color
             )
         b, r = split_red_black(img)
-        epd.init()
-        epd.display(epd.getbuffer(b), epd.getbuffer(r))
-        epd.sleep()
+        #epd.init()
+        #epd.display(epd.getbuffer(b), epd.getbuffer(r))
+        #epd.sleep()
         img.save(buffer, format="png")
     return base64.b64encode(buffer.getvalue()).decode("utf-8")
 
 def state():
-    global epddata
+    epddata = read_epddata()
     return jsonify({ "status": True, "data": epddata })
